465ac36b741464fbc8180b9800b10be51a956b62
BawaRC <98166667+BawaRC@users.noreply.github.com>
2024-12-05 01:36:51 +0530

    Update settings.py

--- WeatherProj/WeatherProj/settings.py
+++ WeatherProj/WeatherProj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9)krqz+ikrp0i7o4xiee7@mhsy#jm!=(jioek20@n)4zdju(mb'
+SECRET_KEY = 'production_key_here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
